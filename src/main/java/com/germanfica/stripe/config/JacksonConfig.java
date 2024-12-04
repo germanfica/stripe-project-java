60396fb816b7b36e2cd841b167102d7354f3127a
German Fica <germafica@gmail.com>
2024-12-04 17:03:35 -0300

    Add JacksonConfig

---
+++ src/main/java/com/germanfica/stripe/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package com.germanfica.stripe.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // Excluir valores null
+        return objectMapper;
+    }
+}
