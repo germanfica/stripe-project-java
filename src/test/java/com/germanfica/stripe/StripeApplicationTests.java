b32dd1e9843f058b5581cc23a7d2f5356352d8cf
German Fica <germafica@gmail.com>
2024-12-04 14:05:55 -0300

    Initial commit

---
+++ src/test/java/com/germanfica/stripe/StripeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanfica.stripe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
